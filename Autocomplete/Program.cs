44081fe68f320618adf4ffe52e979c02f6a0d0e6
archiii92 <den92n@yandex.ru>
2018-04-26 20:10:20 +0400

    init commit

---
+++ Autocomplete/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Autocomplete
+{
+    class Program
+    {
+        static void Main()
+        { 
+            List<string> dictionary = new List<string>();
+            List<string> autocomplete = new List<string>();
+            bool isDictionaryFill = true;
+
+            using (StreamReader reader = new StreamReader("Input.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line != "===")
+                    {
+                        if (isDictionaryFill)
+                        {
+                            dictionary.Add(line);
+                            Console.WriteLine(line);
+                        }
+                        else
+                        {
+                            autocomplete.Add(line);
+                            Console.WriteLine(line);
+                        }
+                    }
+                    else
+                    {
+                        isDictionaryFill = false;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
